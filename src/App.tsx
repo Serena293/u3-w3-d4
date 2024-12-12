bc9d26f420638145ed812c37c9c7957f4f4788ef
SERENA FERRARO <serenaferraro21@gmail.com>
2024-12-12 14:19:28 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import MyNavbar from './components/MyNavbar'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <MyNavbar/>
+     </>
+  )
+}
+
+export default App
